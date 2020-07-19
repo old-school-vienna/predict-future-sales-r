14bfb5524d8440cf288e7ed44e825e4afba0570e
wwagner4 <wwagner4@gmail.com>
2020-07-19 12:14:10 +0200

    shopanalyse verbessert

--- ShopAnalyse.R
+++ ShopAnalyse.R
@@ -1,20 +1,25 @@
-install.packages("tidyverse")
-install.packages("ggplot2")
+# Run Pred01.R until df_sales3$shopitem_id = paste(as.ch... line 45 to create the data you need
 
-df_shops = aggregate(data.frame(df_sales3$item_cnt_month), by=data.frame(df_sales3$shop_id, df_sales3$date_block_num), sum, na.rm=TRUE)
+df_shops = aggregate(data.frame(df_sales3$item_cnt_month), by=data.frame(df_sales3$shop_id, df_sales3$month), sum, na.rm=TRUE)
 colnames(df_shops) = c("shop_id", "month", "item_cnt_month")
 
 df_shops_sum = aggregate(data.frame(df_shops$item_cnt_month), by=data.frame(df_shops$shop_id), sum)
-colnames(df_shops_sum) = c("shop_id", "item_cnt_month")
+colnames(df_shops_sum) = c("shop_id", "sum")
 
 df_shops_cnt = aggregate(data.frame(df_shops$item_cnt_month), by=data.frame(df_shops$shop_id), length)
-colnames(df_shops_cnt) = c("shop_id", "item_cnt_month")
+colnames(df_shops_cnt) = c("shop_id", "cnt")
 
-df_shops = merge(x = df_shops, y = df_shops_sum, by = "shop_id", all = TRUE)
-df_shops = df_shops[with(df_shops, order(item_cnt_month.y)), ]
+df_shops = merge(x = df_shops, y = df_shops_sum, by = "shop_id", all.x = TRUE)
+names(df_shops)
+df_shops = merge(x = df_shops, y = df_shops_cnt, by = "shop_id", all.x = TRUE)
+names(df_shops)
 
-
-ggplot(df_shops, aes(x = reorder(shop_id, item_cnt_month.y), y = item_cnt_month.x)) +
+ggplot(df_shops, aes(x = reorder(shop_id, sum), y = item_cnt_month)) +
   geom_point() +
-  labs(title="sales of shops / sum of products", x='shop_id') 
+  labs(title="sales of shops / sum of products", x='shop_id')
+
+ggplot(df_shops, aes(x = reorder(shop_id, cnt), y = item_cnt_month)) +
+  geom_point() +
+  labs(title="sales of shops / number of products", x='shop_id')
+
 
