fa0fe71581d6f448eff111fee1a2cc76780903bc
wwagner4 <wwagner4@gmail.com>
2020-07-19 12:46:55 +0200

    Pfad für mac hinzugefügt

--- Create_TrainMonthly_01.R
+++ Create_TrainMonthly_01.R
@@ -2,7 +2,9 @@ library(reshape2)
 
 set.seed(12345)
 
-Pathcsv = "C://Users//karlb//Documents//SASUniversityEdition//myfolders//Predict Future Sales//"
+# Pathcsv = "/data/kaggle/pfs/" # ww ben
+Pathcsv = "/Users/wwagner4/work/kaggle/pfs/" # ww mac
+# Pathcsv = "C://Users//karlb//Documents//SASUniversityEdition//myfolders//Predict Future Sales//"
 # Pathcsv = "C://Users//Win10//Documents//SASUniversityEdition//myfolders//Predict Future Sales//" # Intel NUK
 
 csvfile = paste(Pathcsv, "sales_train.csv", sep="")
